ef1c9fa391b5889bb11cb478cf4ee77e30a342af
SoonhanSaram <kyengmin911@naver.com>
2023-02-08 11:19:42 +0900

    0208 naver

---
+++ SpMVC_007_myBooks/src/main/java/com/kyoungmin/mybooks/config/NaverSecretSample.java
@@ -0,0 +1,7 @@
+package com.kyoungmin.mybooks.config;
+
+public class NaverSecretSample {
+	public final static String NAVER_ID = "";
+	public final static String NAVER_SECRET = "";
+	public final static String NAVER_BOOK_URL_JSON = "https://openapi.naver.com/v1/search/book.json";
+}
